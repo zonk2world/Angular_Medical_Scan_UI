80259407e76fd7ba04bee4290725ce141da05cd6
zonk2world <zonk2world@gmail.com>
2022-05-10 08:41:27 -0400

    clean up

--- environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  baseUrl: 'http://18.206.227.117:8080/',
-  outputfolderpath:"http://18.206.227.117:8887/Output/",
-  inputfolderpath:"http://18.206.227.117:8887/Input/",
-  temppath:"http://18.206.227.117:8887/Temp/tempVideo/",
-  expdate:null,
-  isExp:true
-};
