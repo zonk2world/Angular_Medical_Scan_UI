88f57cd7d5ebc7aeeb3f2eac13f82ec38b852743
zonk2world <zonk2world@gmail.com>
2023-02-07 13:18:06 -0500

    cropped video feature cleanup

--- app/approvecroppedvideo/approvecroppedvideo.component.ts
+++ app/approvecroppedvideo/approvecroppedvideo.component.ts
@@ -1,46 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { VideoplayerService } from 'src/shared/videoplayer.service';
-import { Router } from '@angular/router';
-import * as $ from 'jquery';
-import { CropvideoService } from '../cropvideo/cropvideo.service';
+import { Component, OnInit } from "@angular/core";
+import { VideoplayerService } from "src/shared/videoplayer.service";
+import { Router } from "@angular/router";
+import * as $ from "jquery";
+import { CropvideoService } from "../cropvideo/cropvideo.service";
 
 @Component({
-  selector: 'app-approvecroppedvideo',
-  templateUrl: './approvecroppedvideo.component.html',
-  styleUrls: ['./approvecroppedvideo.component.css']
+  selector: "app-approvecroppedvideo",
+  templateUrl: "./approvecroppedvideo.component.html",
+  styleUrls: ["./approvecroppedvideo.component.css"],
 })
 export class ApprovecroppedvideoComponent implements OnInit {
-
-  constructor(private _router:Router,private _cropvideoService: CropvideoService, private _videoplayerService: VideoplayerService) { }
+  constructor(
+    private _router: Router,
+    private _cropvideoService: CropvideoService,
+    private _videoplayerService: VideoplayerService
+  ) {}
   Overlaystyle: string = "none";
   divApprove = false;
   ngOnInit() {
-    this.Overlaystyle = 'block';
+    this.Overlaystyle = "block";
     this.getPreviewImages();
-    this.Overlaystyle = 'none';
+    this.Overlaystyle = "none";
   }
   getPreviewImages() {
-
-    this._cropvideoService.GetImages(localStorage.getItem("uploadedfile"), "2").subscribe(
-      imageslist => {
-        if (imageslist !== undefined || imageslist !== null) {
-          this.divApprove = true;
-          this._videoplayerService.processVideo(imageslist,false);
-          $('#divplay').trigger('click');
-
-        }
-        else {
-          // this.Overlaystyle = 'none';
-        }
-      }//get error
-
-    )
+    this._cropvideoService
+      .GetImages(localStorage.getItem("uploadedfile"), "2")
+      .subscribe(
+        (imageslist) => {
+          if (imageslist !== undefined || imageslist !== null) {
+            this.divApprove = true;
+            this._videoplayerService.processVideo(imageslist, false);
+            $("#divplay").trigger("click");
+          } else {
+            // this.Overlaystyle = 'none';
+          }
+        } //get error
+      );
   }
   approve() {
-    this._router.navigate(['guideframe']);
-    
+    this._router.navigate(["guideframe"]);
   }
-  cancel() { 
-    this._router.navigate(['cropvideo']);
+  cancel() {
+    this._router.navigate(["cropvideo"]);
   }
 }
